819bce4e48fa583254efa809e6455ab80dae21a8
bidishaMondal7 <bidisha.tuli7@gmail.com>
2022-01-15 01:33:58 +0530

    Styled home comp

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,25 @@
 import { Link } from "react-router-dom";
+import { Card, Button } from "react-bootstrap";
+import './HomeStyle.css';
 
 const Home = () => {
-  return <Link to="/list">User List</Link>;
+  return (
+    <div class="container">
+      <div class="card-content">
+      <Card >
+        <Card.Body>
+          <Card.Title>Hey!</Card.Title>
+          <Card.Text>
+            Lets get you updated..
+          </Card.Text>
+            <Link style={{"text-decoration" : "none"}} to="/list">Click to view List</Link>    
+            
+        </Card.Body>
+      </Card>
+     <div class="bouncingball"></div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
